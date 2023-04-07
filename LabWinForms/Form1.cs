a54eab9d2b079fa3dd4d28856377039287653af6
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2023-04-07 12:39:11 +0300

    Lab1

---
+++ LabWinForms/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace LabWinForms
+{
+    public partial class Form1 : Form
+    {
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader file = new StreamReader("1.txt");
+            string f="No words";
+            if (file != null)
+            {
+                 f= file.ReadToEnd();
+            }
+            
+            textBox1.Text = f;
+
+            progressBar1.Value = f.Count();
+            if(textBox1.Text.Length > 1000)
+            {
+                progressBar1.Value = progressBar1.Maximum;
+            }
+            file.Close();
+        }
+    }
+}
