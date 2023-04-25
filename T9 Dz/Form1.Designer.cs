f0fb817aba707e422cca45ac71228c88343df7a4
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2023-04-25 21:34:56 +0300

    T9 Dz

---
+++ T9 Dz/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace T9_Dz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AutoWordSelection = true;
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(9, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(288, 184);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(229, 279);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(68, 21);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(12, 280);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(60, 19);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "C";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(128, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2\r\nАБВГ\r\nABC";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 306);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 53);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3\r\nДЕЖЭ\r\nDEF";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(66, 364);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 53);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4\r\nИЙКЛ\r\nGHI";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(128, 364);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(61, 53);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "5 \r\nМНОП\r\nJKL";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(195, 364);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(61, 53);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "6\r\nРСТУ\r\nMNO";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(66, 423);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(56, 53);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "7 \r\nФХСЧ\r\nPQRS";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(128, 423);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(61, 53);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "8\r\nШЩЬЫ\r\nTUV";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(195, 423);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(61, 53);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "9\r\nЬЭЮЯ\r\nWXYZ";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(97, 231);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(49, 33);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "<--";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(167, 231);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(49, 33);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "-->";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(128, 482);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(61, 42);
+            this.button12.TabIndex = 5;
+            this.button12.Text = "0";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(75, 202);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(164, 22);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 525);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Телефон";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ T9 Dz/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace T9_Dz
+{
+    public partial class Form1 : Form
+    {
+        string current_text;
+      
+        int Kol = 0;
+        int timer = 0;
+        string cur_text;
+
+        string New_word;
+        Mutex mutex=new Mutex(true,"Mutex");
+        List<string> Dictionary = new List<string>();
+
+        List<string> Button1 = new List<string>() { "1" };
+        List<string> Button2 = new List<string>() { "2","А","Б","В","Г","A","B","C" };
+        List<string> Button3 = new List<string>() { "3","Д","Е","Ж,","Э","D","E","F" };
+        List<string> Button4 = new List<string>() { "4", "З", "И", "Й", "К", "L", "M", "N" };
+        List<string> Button5 = new List<string>() { "5", "Л", "М", "Н", "О", "O", "P", "Q" };
+        List<string> Button6 = new List<string>() { "6", "П", "Р", "С", "Т", "U", "V", "W" };
+        List<string> Button7 = new List<string>() { "7", "У", "Ф", "Х", "Ц", "X", "Y", "Z" };
+        List<string> Button8 = new List<string>() { "8", "Ч", "Ш", "Щ", "Ъ", ".", ",", "?" };
+        List<string> Button9 = new List<string>() { "9", "Ы", "Ь", "Э", "Ю", "-", "@", "!" };
+        List<string> Button0 = new List<string>() { "0", ".", " ", "_", ":", ";", "(", ")" };
+
+    
+
+
+        public Form1()
+        {
+            
+            
+            Dictionary.Add("автомобиль");
+            Dictionary.Add("книга");
+            Dictionary.Add("музыка");
+            Dictionary.Add("компьютер");
+            Dictionary.Add("здание");
+            Dictionary.Add("фильм");
+            Dictionary.Add("стол");
+            Dictionary.Add("ручка");
+            Dictionary.Add("растение");
+            Dictionary.Add("зеркало");
+            InitializeComponent();
+        }
+
+
+        private void SelectNewText()
+        {
+            
+            richTextBox1.Select(richTextBox1.Text.Count() - 1, 1);
+         
+
+        }
+        private void Suggest(string word)
+        {
+            foreach (var item in Dictionary)
+            {
+                if (item.StartsWith(word.ToLower()))
+                {
+                    richTextBox2.Text = item;
+                }
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mutex.WaitOne(); 
+           
+            if (timer1.Enabled == true)
+            {
+                richTextBox1.Text = current_text;
+            }
+            
+            richTextBox1.Text += Button1[Kol];
+            
+            current_text = richTextBox1.Text;
+           
+            SelectNewText(); 
+            mutex.ReleaseMutex();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mutex.WaitOne();
+            
+            if (timer1.Enabled == true)
+            {
+                richTextBox1.Text = current_text;
+            }
+            else
+            {
+                Kol = 0;
+            }
+
+            if (Kol >= Button2.Count - 1)
+            {
+                Kol = 0;
+            }
+            else
+            {
+                Kol++;
+
+            }
+            richTextBox1.Text += Button2[Kol];
+           
+            SelectNewText();
+
+          
+
+           
+            mutex.ReleaseMutex(); 
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Start();
+            timer2.Start();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            timer++;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mutex.WaitOne();
+
+            if (timer1.Enabled == true)
+            {
+                richTextBox1.Text = current_text;
+            }
+            else
+            {
+                Kol = 0;
+            }
+            if (Kol >= Button3.Count-1)
+            {
+                Kol = 0;
+            }
+            else
+            {
+                Kol++;
+
+            }
+            richTextBox1.Text += Button3[Kol];
+          
+
+            SelectNewText();
+            mutex.ReleaseMutex();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mutex.WaitOne();
+
+            if (timer1.Enabled == true)
+            {
+                richTextBox1.Text = current_text;
+            }
+            else
+            {
+                Kol = 0;
+            }
+            if (Kol >= Button4.Count-1)
+            {
+                Kol = 0;
+            }
+            else
+            {
+                Kol++;
+
+            }
+            richTextBox1.Text += Button4[Kol];
+        
+            
+            SelectNewText();
+            mutex.ReleaseMutex();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            mutex.WaitOne();
+
+            if (timer1.Enabled == true)
+            {
+                richTextBox1.Text = current_text;
+            }
+            else
+            {
+                Kol = 0;
+            }
+            if (Kol >= Button5.Count-1)
+            {
+                Kol = 0;
+            }
+            else
+            {
+                Kol++;
+
+            }
+            richTextBox1.Text += Button5[Kol];
+         
+
+            SelectNewText();
+            mutex.ReleaseMutex();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            mutex.WaitOne();
+
+            if (timer1.Enabled == true)
+            {
+                richTextBox1.Text = current_text;
+            }
+            else
+            {
+                Kol = 0;
+            }
+            if (Kol >= Button6.Count - 1)
+            {
+                Kol = 0;
+            }
+            else
+            {
+                Kol++;
+
+            }
+            richTextBox1.Text += Button6[Kol];
+       
+
+            SelectNewText();
+            mutex.ReleaseMutex();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            mutex.WaitOne();
+
+            if (timer1.Enabled == true)
+            {
+                richTextBox1.Text = current_text;
+            }
+            else
+            {
+                Kol = 0;
+            }
+            if (Kol >= Button7.Count-1)
+            {
+                Kol = 0;
+            }
+            else
+            {
+                Kol++;
+
+            }
+            richTextBox1.Text += Button7[Kol];
+        
+
+            SelectNewText();
+            mutex.ReleaseMutex();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            mutex.WaitOne();
+
+            if (timer1.Enabled == true)
+            {
+                richTextBox1.Text = current_text;
+            }
+            else
+            {
+                Kol = 0;
+            }
+            if (Kol >= Button8.Count-1)
+            {
+                Kol = 0;
+            }
+            else
+            {
+                Kol++;
+
+            }
+            richTextBox1.Text += Button8[Kol];
+        
+
+            SelectNewText();
+            mutex.ReleaseMutex();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            mutex.WaitOne();
+
+            if (timer1.Enabled == true)
+            {
+                richTextBox1.Text = current_text;
+            }
+            else
+            {
+                Kol = 0;
+            }
+            if (Kol >= Button9.Count-1)
+            {
+                Kol = 0;
+            }
+            else
+            {
+                Kol++;
+
+            }
+            richTextBox1.Text += Button9[Kol];
+        
+
+            SelectNewText();
+            mutex.ReleaseMutex();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (timer > 2) // если текст выделен
+            {
+                timer = 0;
+                mutex.WaitOne();
+                cur_text += richTextBox1.SelectedText;
+                Suggest(cur_text);
+                richTextBox1.Select(0, 0);
+                Kol = 0;
+                current_text = richTextBox1.Text;
+                timer1.Stop();
+              
+                mutex.ReleaseMutex();
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += " ";
+            cur_text = "";
+            richTextBox2.Clear();
+            New_word = richTextBox1.Text;
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (richTextBox2.Text.Length != 0)
+            {
+                richTextBox1.Text = New_word;
+                richTextBox1.Text += richTextBox2.Text;
+               current_text+=richTextBox1.Text;
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            New_word = " ";
+            cur_text = " ";
+            current_text = " ";
+        }
+    }
+
+}
+
+
+
+
+
