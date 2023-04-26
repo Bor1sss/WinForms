912e82d24f5db9166d4356090f4d172f03525325
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2023-04-26 14:30:23 +0300

    AsyncAwait

---
+++ AsyncAwait/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace AsyncAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Список";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(307, 381);
+            this.listBox1.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 40);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Task1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 87);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Task2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 143);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Task3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 185);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(55, 17);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Task4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 230);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(55, 17);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Task5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(353, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 271);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(95, 227);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(143, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(95, 182);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(143, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(95, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(143, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(95, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(95, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 501);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ AsyncAwait/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AsyncAwait
+{
+    public partial class Form1 : Form
+    {
+        List<Employers> employers;
+        public int cur;
+        public SynchronizationContext uiContext;
+        List<Student> students = new List<Student>()
+{
+    new Student()
+    {
+        Name = "Иванов Иван Иванович",
+        GroupNumber = "Группа 123",
+        PhysicsGrade = 4,
+        MathGrade = 5,
+        ComputerScienceGrade = 5,
+        LastExamDate = new DateTime(2023, 4, 20)
+    },
+    new Student()
+    {
+        Name = "Петров Петр Петрович",
+        GroupNumber = "Группа 123",
+        PhysicsGrade = 5,
+        MathGrade = 4,
+        ComputerScienceGrade = 4,
+        LastExamDate = new DateTime(2023, 4, 20)
+    },
+    new Student()
+    {
+        Name = "Сидорова Анна Ивановна",
+        GroupNumber = "Группа 124",
+        PhysicsGrade = 3,
+        MathGrade = 5,
+        ComputerScienceGrade = 5,
+        LastExamDate = new DateTime(2023, 4, 21)
+    },
+    new Student()
+    {
+        Name = "Иванова Ольга Петровна",
+        GroupNumber = "Группа 124",
+        PhysicsGrade = 4,
+        MathGrade = 4,
+        ComputerScienceGrade = 5,
+        LastExamDate = new DateTime(2023, 4, 21)
+    },
+    new Student()
+    {
+        Name = "Петрова Екатерина Сергеевна",
+        GroupNumber = "Группа 125",
+        PhysicsGrade = 5,
+        MathGrade = 5,
+        ComputerScienceGrade = 4,
+        LastExamDate = new DateTime(2023, 4, 22)
+    },
+    new Student()
+    {
+        Name = "Сидоров Иван Андреевич",
+        GroupNumber = "Группа 125",
+        PhysicsGrade = 4,
+        MathGrade = 4,
+        ComputerScienceGrade = 4,
+        LastExamDate = new DateTime(2023, 4, 22)
+    },
+    new Student()
+    {
+        Name = "Кузнецов Павел Владимирович",
+        GroupNumber = "Группа 126",
+        PhysicsGrade = 3,
+        MathGrade = 4,
+        ComputerScienceGrade = 5,
+        LastExamDate = new DateTime(2023, 4, 23)
+    },
+    new Student()
+    {
+        Name = "Никитина Анна Александровна",
+        GroupNumber = "Группа 126",
+        PhysicsGrade = 5,
+        MathGrade = 5,
+        ComputerScienceGrade = 5,
+        LastExamDate = new DateTime(2023, 4, 23)
+    },
+    new Student()
+    {
+        Name = "Антонов Игорь Викторович",
+        GroupNumber = "Группа 127",
+        PhysicsGrade = 4,
+        MathGrade = 5,
+        ComputerScienceGrade = 3,
+        LastExamDate = new DateTime(2023, 4, 24)
+    } };
+ 
+        public Form1()
+        {
+            InitializeComponent();
+            uiContext = SynchronizationContext.Current;
+            employers =new List<Employers>();
+
+            Employers emp1 = new Employers("John", "Smith", "123 Main Street", new DateTime(1985, 3, 15));
+            employers.Add(emp1);
+
+            Employers emp2 = new Employers("Mary", "Johnson", "456 Oak Avenue", new DateTime(1990, 6, 28));
+            employers.Add(emp2);
+
+            Employers emp3 = new Employers("Peter", "Lee", "789 Pine Street", new DateTime(1982, 2, 10));
+            employers.Add(emp3);
+
+            Employers emp4 = new Employers("Sarah", "Kim", "321 Elm Street", new DateTime(1995, 4, 3));
+            employers.Add(emp4);
+
+            Employers emp5 = new Employers("Michael", "Chen", "654 Maple Avenue", new DateTime(1988, 11, 20));
+            employers.Add(emp5);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                listBox1.Items.Clear();
+                cur = 1;
+                textBox1.Enabled = true;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                textBox5.Enabled = false;
+                foreach (var item in employers)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+            else if(radioButton2.Checked)
+            {
+                listBox1.Items.Clear();
+                cur = 2;
+                textBox1.Enabled = false;
+                textBox2.Enabled = true;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                textBox5.Enabled = false;
+                foreach (var item in employers)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+            else if (radioButton3.Checked)
+            {
+                listBox1.Items.Clear();
+                cur = 3;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = true;
+                textBox4.Enabled = false;
+                textBox5.Enabled = false;
+
+                foreach (var item in students)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+            else if (radioButton4.Checked)
+            {
+                listBox1.Items.Clear();
+                cur = 4;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = true;
+                textBox5.Enabled = false;
+            }
+            else if (radioButton5.Checked)
+            {
+                listBox1.Items.Clear();
+                cur = 5;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                textBox5.Enabled = true;
+            }
+
+
+        }
+
+    
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            switch (cur)
+            {
+                case 1:
+                    await Task.Run(() =>
+                    {
+
+                        foreach (var item in employers)
+                        {
+                            if (item.Address.Contains(textBox1.Text))
+                            {
+                                uiContext.Send(d => listBox1.Items.Clear(), null);
+
+                                uiContext.Send(d => listBox1.Items.Add(item), null);
+
+                                // Подсчёт среднего возраста дополнительно
+
+                            }
+
+                        }
+
+                    }); break;
+                case 2:
+                    await Task.Run(() =>
+                    {
+
+                        foreach (var item in employers)
+                        {
+                            if (item.DateBirthday.ToString().Contains(textBox2.Text))
+                            {
+                                uiContext.Send(d => listBox1.Items.Clear(), null);
+
+                                uiContext.Send(d => listBox1.Items.Add(item), null);
+                            }
+
+                        }
+
+                    }); break;
+                case 3:
+                    await Task.Run(() =>
+                    {
+
+                        foreach (var item in students)
+                        {
+                            if (item.LastExamDate.ToString().Contains(textBox3.Text))
+                            {
+                                uiContext.Send(d => listBox1.Items.Clear(), null);
+
+                                uiContext.Send(d => listBox1.Items.Add(item), null);
+                            }
+
+                        }
+
+                    }); break;
+                case 4:
+                    await Task.Run(() =>
+                    {
+
+                        foreach (var item in employers)
+                        {
+                            if (item.Address.Contains(textBox1.Text))
+                            {
+                                uiContext.Send(d => listBox1.Items.Clear(), null);
+
+                                uiContext.Send(d => listBox1.Items.Add(item), null);
+                            }
+
+                        }
+
+                    }); break;
+                case 5:
+                    await Task.Run(() =>
+                    {
+
+                        foreach (var item in employers)
+                        {
+                            if (item.Address.Contains(textBox1.Text))
+                            {
+                                uiContext.Send(d => listBox1.Items.Clear(), null);
+
+                                uiContext.Send(d => listBox1.Items.Add(item), null);
+                            }
+
+                        }
+
+                    }); break;
+
+
+                default:
+                    break;
+            }
+            
+      
+          
+        }
+    }
+
+    public class Employers
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public DateTime DateBirthday { get; set; }
+
+        public Employers(string name, string surname, string address, DateTime dateBirthday)
+        {
+            Name = name;
+            Surname = surname;
+            Address = address;
+            DateBirthday = dateBirthday;
+        }
+        public string GetInitals()
+        {
+            return $"{Address}  Date {DateBirthday}";
+           
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Surname}  ||  {GetInitals()}";
+        }
+
+    }
+
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string GroupNumber { get; set; }
+        public int PhysicsGrade { get; set; }
+        public int MathGrade { get; set; }
+        public int ComputerScienceGrade { get; set; }
+        public DateTime LastExamDate { get; set; }
+        public override string ToString()
+        {
+            return $"ФИО: {Name}\n" +
+                   $"Номер группы: {GroupNumber}\n" +
+                   $"Оценка по физике: {PhysicsGrade}\n" +
+                   $"Оценка по математике: {MathGrade}\n" +
+                   $"Оценка по информатике: {ComputerScienceGrade}\n" +
+                   $"Дата сдачи последнего экзамена: {LastExamDate.ToShortDateString()}";
+        }
+
+    }
+
+}
